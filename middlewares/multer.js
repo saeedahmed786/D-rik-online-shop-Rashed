d8730eec8e541e385a9910c2c4847ae5a224cd17
devuplio <119362834+devuplio@users.noreply.github.com>
2023-10-20 07:22:52 +0500

    last

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,18 +1,41 @@
+// const multer = require('multer');
+
+
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, './uploads/') 
+//     },
+
+//     filename: function(req, file, cb) {
+//         cb(null, Date.now() + '-' + file.originalname)
+//     }
+// });
+
+// const upload =  multer({
+//     storage : storage
+// });
+
+// module.exports = upload;
+
 const multer = require('multer');
+const { join } = require('path');
 
-
+// Define the storage settings for multer
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './uploads/') 
+        // Use the /tmp directory for temporary storage (writable in Vercel)
+        cb(null, join(process.cwd(), '/tmp/'));
     },
-
-    filename: function(req, file, cb) {
-        cb(null, Date.now() + '-' + file.originalname)
+    filename: function (req, file, cb) {
+        // Define a custom filename for each uploaded file (e.g., with a timestamp)
+        cb(null, Date.now() + '-' + file.originalname);
     }
 });
 
-const upload =  multer({
-    storage : storage
+// Create a multer instance with the defined storage settings
+const upload = multer({
+    storage: storage
 });
 
+// Export the upload middleware to be used in your routes or controllers
 module.exports = upload;
